9435d24c165abcaeea2c6491c64cdbbcd2ac2c87
texttoolgame <vantaynguyen0@gmail.com>
2022-03-04 17:28:05 +0700

    commit

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,12 +1,17 @@
-import React from "react";
-import { FlexboxUl, Gird02li, Gird03li, Gird07li, Flexboxdiv, Gird02div, Gird06div, Gird04div, Gird05div  } from "../styles/GirdComponents.js"; 
-import { ContainerFull, ContainerFluid } from "../styles/StyledComponents.js";
-import { AiOutlineSearch, AiOutlineUser, AiFillCaretDown, AiOutlineShoppingCart} from "react-icons/ai";
-
+import React, {useState} from "react";
+import { AiOutlineSearch, AiOutlineUser, AiFillCaretDown, AiOutlineShoppingCart, AiOutlineHome} from "react-icons/ai";
+import { ContainerFull, ContainerFluid, FlexboxUl, Gird02li, Gird03li, Gird07li, Flexboxdiv, Gird02div, Gird06div, Gird04div, Gird08div, Gird04divOffset  } from "../styles/StyledComponents.js";
 function NavBar() {
+    const [classLoginForm, setClassLoginForm] = useState();
+    const openFormLogin = () =>{
+        setClassLoginForm(true)
+    }
+    const closeFormLogin = ()=> {
+        setClassLoginForm()
+    }
     return (
         <>
-            <ContainerFull>
+            <ContainerFull className="Navbox">
                 <ContainerFluid>
                     <FlexboxUl padding=" 20px 0" flexDecoration="row">
                         <Gird02li>
@@ -28,7 +33,7 @@ function NavBar() {
                                         Tìm kiếm
                                     </button>
                                 </form>
-                                <div className="adsFormSearch">
+                                <div className="adsFormSearch fontSize12px textWhite">
                                     <a href="">trái cây</a>
                                     <a href="">thịt, trứng</a>
                                     <a href="">rau củ quả</a>
@@ -40,13 +45,15 @@ function NavBar() {
                                 </div>
                             </div>
                         </Gird07li>
+                        
                         <Gird03li className="textWhite">
                             <Flexboxdiv className="loginBox">
-                                <Gird02div className="iconUser">
+                                <button onClick={openFormLogin}  className="btnLoginForm"></button>
+                                <Gird02div className="iconUserAndCart">
                                     <AiOutlineUser/>
                                 </Gird02div>
                                 <Gird06div>
-                                    <div className="linkLogin">
+                                    <div className="linkLogin fontSize12px">
                                         Đăng Nhập/Đăng Ký
                                     </div>
                                     <div clas>
@@ -56,23 +63,47 @@ function NavBar() {
                                 </Gird06div>
                                 <Gird04div>
                                     <Flexboxdiv flexDecoration="space-between">
-                                        <Gird06div className="iconUser">
+                                        <Gird06div className="iconUserAndCart">
                                             <AiOutlineShoppingCart/>
+                                            <p className="countProductIncart fontSize12px">0</p>
                                         </Gird06div>
-                                        <Gird06div className="  ">
+                                        <Gird06div className="cartTitle fontSize12px">
                                             Giỏ hàng
                                         </Gird06div>
                                     </Flexboxdiv>
                                 </Gird04div>
                             </Flexboxdiv>
-                            <div>
-                                bán hàng cùng tiki
-                            </div>
+                            <Flexboxdiv justyfyContent="space-between">
+                                <Gird04divOffset></Gird04divOffset>
+                                <Gird06div className="resgiterSeller">
+                                    <AiOutlineHome/>
+                                    <p>
+                                        Bán Hàng Cùng TiKi
+                                    </p>
+                                </Gird06div>
+                            </Flexboxdiv>
                         </Gird03li>
                     </FlexboxUl>
                 </ContainerFluid>
+                <div onClick={closeFormLogin}  className={`loginFormBox ${classLoginForm ? 'loginFormBoxOpen' : ''}`}>
+                    <Gird08div>
+                        <Flexboxdiv>
+                            <Flexboxdiv className="formBoxLogin">
+                                <Gird08div>
+                                    <h1>
+                                        Xin Chào
+                                    </h1>
+                                </Gird08div>
+                                <Gird04div>
+                                    Mua sắm tại tiki
+                                </Gird04div>
+                            </Flexboxdiv>
+                        </Flexboxdiv>
+                    </Gird08div>
+                   
+                </div>
             </ContainerFull>
         </>
     )
 }
-export default NavBar;
+export default NavBar ;
