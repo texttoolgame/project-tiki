eead000e774307b6adb39e78864beb29b2d3fb1b
texttoolgame <vantaynguyen0@gmail.com>
2022-03-07 11:03:26 +0700

    commit

--- styles/GirdComponents12.js
+++ styles/GirdComponents12.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
-export const FlexboxUl = styled.ul`
+export const Flexbox12iUl = styled.ul`
     display: flex;
     flex-direction: ${prop => prop.flexDecoration};
     list-style-type: none;
-    height: ${prop => prop.height};
+        height: ${prop => prop.height};
     width: ${prop => prop.width};
     padding : ${prop => prop.padding};
     margin: ${prop => prop.margin};
@@ -13,89 +13,89 @@ export const FlexboxUl = styled.ul`
     justify-content: ${prop => prop.justyfyContent};
     justify-items: ${prop => prop.justifyItem};
 `
-export const Gird01li = styled.li`
+export const Gird12i01li = styled.li`
     flex-basis: 08.3334%;
     max-width: 08.3334%;
 `
-export const Gird02li = styled.li`
+export const Gird12i02li = styled.li`
     flex-basis: 16.6668%;
     max-width: 16.6668%;
 `
-export const Gird03li = styled.li`
+export const Gird12i03li = styled.li`
     flex-basis: 25.0002%;
     max-width: 25.0002%;
 `
-export const Gird04li = styled.li`
+export const Gird12i04li = styled.li`
     flex-basis: 33.3336%;
     max-width: 33.3336%;
 `
-export const Gird05li = styled.li`
+export const Gird12i05li = styled.li`
     flex-basis: 41.667%;
     max-width: 41.667%;
 `
-export const Gird06li = styled.li`
+export const Gird12i06li = styled.li`
     flex-basis: 50.0004%;
     max-width: 50.0004%;
 `
-export const Gird07li = styled.li`
+export const Gird12i07li = styled.li`
     flex-basis: 58.3338%;
     max-width: 58.3338%;
 `
-export const Gird08li = styled.li`
+export const Gird12i08li = styled.li`
     flex-basis: 66.6672%;
     max-width: 66.6672%;
 `
-export const Gird09li = styled.li`
+export const Gird12i09li = styled.li`
     flex-basis: 75.006%;
     max-width: 75.006%;
 `
-export const Gird10li = styled.li`
+export const Gird12i10li = styled.li`
     flex-basis: 83.334%;
     max-width: 83.334%;
 `
-export const Gird11li = styled.li`
+export const Gird12i11li = styled.li`
     flex-basis: 91.6674%;
     max-width: 91.6674%;
 `
-export const Gird12li = styled.li`
+export const Gird12i12li = styled.li`
     flex-basis: 100%;
     max-width: 100%;
 `
-export const Girdli02Offset = styled.li`
+export const Gird12ili02Offset = styled.li`
     margin-left: 08.3334%;
 `
-export const Girdli03Offset = styled.li`
+export const Gird12ili03Offset = styled.li`
     margin-left: 16.6668%;
 `
-export const Girdli04Offset = styled.li`
+export const Gird12ili04Offset = styled.li`
     margin-left: 25.0002%;
 `
-export const Girdli05Offset = styled.li`
+export const Gird12ili05Offset = styled.li`
     margin-left: 41.667%;
 `
-export const Girdli06Offset = styled.li`
+export const Gird12ili06Offset = styled.li`
     margin-left: 50.0004%;
 `
-export const Girdli07Offset = styled.li`
+export const Gird12ili07Offset = styled.li`
     margin-left: 58.3338%;
 `
-export const Girdli08Offset = styled.li`
+export const Gird12ili08Offset = styled.li`
     margin-left: 66.6672%;
 `
-export const Girdli09Offset = styled.li`
+export const Gird12ili09Offset = styled.li`
     margin-left: 75.006%;
 `
-export const Girdli10Offset = styled.li`
+export const Gird12ili10Offset = styled.li`
     margin-left: 83.334%;
 `
-export const Girdli11Offset = styled.li`
+export const Gird12ili11Offset = styled.li`
     margin-left: 91.6674%;
 `
-export const Girdli12Offset = styled.li`
+export const Gird12ili12Offset = styled.li`
     margin-left: 100%;
 `
 // flex box div
-export const Flexboxdiv = styled.div`
+export const Flexbox12idiv = styled.div`
     display: flex;
     flex-direction: ${prop => prop.flexDecoration};
     height: ${prop => prop.height};
@@ -107,88 +107,91 @@ export const Flexboxdiv = styled.div`
     justify-content: ${prop => prop.justyfyContent};
     justify-items: ${prop => prop.justifyItem};
 `
-export const Gird01div = styled.div`
+export const Gird12i01div = styled.div`
     flex-basis: 08.3334%;
     max-width: 08.3334%; 
 `
-export const Gird02div = styled.div`
+export const Gird12i02div = styled.div`
     flex-basis: 16.6668%;
     max-width: 16.6668%;
 `
-export const Gird03div = styled.div`
+export const Gird12i03div = styled.div`
     flex-basis: 25.0002%;
     max-width: 25.0002%;
 `
-export const Gird04div = styled.div`
+export const Gird12i04div = styled.div`
     flex-basis: 33.3336%;
     max-width: 33.3336%;
 `
-export const Gird05div = styled.div`
+export const Gird12i05div = styled.div`
     flex-basis: 41.667%;
     max-width: 41.667%;
 `
-export const Gird06div = styled.div`
+export const Gird12i06div = styled.div`
     flex-basis: 50.0004%;
     max-width: 50.0004%;
 `
-export const Gird07div = styled.div`
+export const Gird12i07div = styled.div`
     flex-basis: 58.3338%;
     max-width: 58.3338%;
 `
-export const Gird08div = styled.div`
+export const Gird12i08div = styled.div`
     flex-basis: 66.6672%;
     max-width: 66.6672%;
 `
-export const Gird09div = styled.div`
+export const Gird12i09div = styled.div`
     flex-basis: 75.006%;
     max-width: 75.006%;
 `
-export const Gird10div = styled.div`
+export const Gird12i10div = styled.div`
     flex-basis: 83.334%;
     max-width: 83.334%;
 `
-export const Gird11div = styled.div`
+export const Gird12i11div = styled.div`
     flex-basis: 91.6674%;
     max-width: 91.6674%;
 `
-export const Gird12div = styled.div`
+export const Gird12i12div = styled.div`
     flex-basis: 100%;
     max-width: 100%;
 `
 // flex Offset div
-export const Gird01divOffset = styled.div`
+export const Gird12i01divOffset = styled.div`
     margin-left: 08.3334%;
 `
-export const Gird02divOffset = styled.div`
-    margin-left: 16.6668%;
-`
-export const Gird03divOffset = styled.div`
+    export const Gird12i02divOffset = styled.(Gird12i01div)`
+        &{
+            margin-left: 16.6668%;
+        }
+        
+    `
+export const Gird12i03divOffset = styled.div`
     margin-left: 25.0002%;
 `
-export const Gird04divOffset = styled.div`
+export const Gird12i04divOffset = styled.div`
     margin-left: 33.3336%;
 `
-export const Gird05divOffset = styled.div`
+export const Gird12i05divOffset = styled.div`
     margin-left: 41.667%;
 `
-export const Gird06divOffset = styled.div`
+export const Gird12i06divOffset = styled.div`
     margin-left: 50.0004%;
 `
-export const Gird07divOffset = styled.div`
+export const Gird12i07divOffset = styled.div`
     margin-left: 58.3338%;
 `
-export const Gird08divOffset = styled.div`
+export const Gird12i08divOffset = styled.div`
     margin-left: 66.6672%;
 `
-export const Gird09divOffset = styled.div`
+export const Gird12i09divOffset = styled.div`
     margin-left: 75.006%;
 `
-export const Gird10divOffset = styled.div`
+export const Gird12i10divOffset = styled.div`
     margin-left: 83.334%;
 `
-export const Gird11divOffset = styled.div`
+export const Gird12i11divOffset = styled.div`
     margin-left: 91.6674%;
 `
-export const Gird12divOffset = styled.div`
+export const Gird12i12divOffset = styled.div`
     margin-left: 100%;
 `
